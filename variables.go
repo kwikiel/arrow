035d3de527d20fab0506b6c3a4db6432ed57b726
Kacper Wikieł <k.wikiel@gmail.com>
2019-10-03 18:36:21 +0200

    Variables

---
+++ variables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "Initial"
+	fmt.Println(a)
+
+	var b int = 1
+	fmt.Println(b)
+
+	//Declaring a variable that won't be used 
+	// 
+	var c int = 21
+	//var d = true // lowercase truth/false
+
+	f := "apple"
+	fmt.Println(f)
+
+	var g = c+1
+	c = g + 3
+
+}
