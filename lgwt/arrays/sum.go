678040928a52d936230b74d804a205cd29ce60f8
Kacper Wikieł <k.wikiel@gmail.com>
2019-10-09 00:30:02 +0200

    Refactor sum of all

--- lgwt/arrays/sum.go
+++ lgwt/arrays/sum.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Sum of array or slice
 func Sum(numbers []int) int {
 	sum := 0
@@ -7,4 +9,20 @@ func Sum(numbers []int) int {
 		sum += number
 	}
 	return sum
+}
+
+// SumAll sums of list of slices
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func main() {
+	var sum = SumAll([]int{1, 2}, []int{0, 9})
+
+	fmt.Println(sum)
 }
