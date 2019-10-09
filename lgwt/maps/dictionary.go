643d7c3b20d04f7621238562e4668214374f8d98
Kacper Wikieł <k.wikiel@gmail.com>
2019-10-09 18:14:41 +0200

    Dictionary Search API

--- lgwt/maps/dictionary.go
+++ lgwt/maps/dictionary.go
@@ -1,10 +1,16 @@
 package main
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
-}
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
 }
